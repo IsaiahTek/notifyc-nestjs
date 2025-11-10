19789b68153a890ceef2c432515be7f9052c1709
Engr., Isaiah Pius <isaiahtech1@gmail.com>
2025-11-10 04:46:14 +0100

    Update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
+export * from '@synq/notifications-core';
 export * from './controllers/notification.controller';
 export * from './gateways/notifications-websocket.gateway';
 export * from './services/notification.service';
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
 "use strict";
-// export * from '@synq/notifications-core';
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
@@ -15,6 +14,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("@synq/notifications-core"), exports);
 __exportStar(require("./controllers/notification.controller"), exports);
 __exportStar(require("./gateways/notifications-websocket.gateway"), exports);
 __exportStar(require("./services/notification.service"), exports);
